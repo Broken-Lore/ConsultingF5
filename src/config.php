66a7f924298611868b2a382ab3f4e255d8df8b45
a-xo <eiekseau@gmail.com>
2021-05-11 16:02:56 +0200

    folder structure added

---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$nameDataBase = "consulting";
+try {
+    $conection = new PDO("mysql:host=$server; dbname=$nameDataBase", $user, $password);      
+} catch(PDOException $error) {
+    echo $error->getMessage();
+};
+?>
+
+<!-- $sql = "SELECT * FROM `appointment`"; -->
