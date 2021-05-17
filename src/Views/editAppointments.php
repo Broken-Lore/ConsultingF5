a38a3716fb01aa6a703688e5959303a7b7ca0d1c
PPIICCAA <abdulhaidrees@gmail.com>
2021-05-17 13:48:27 +0200

    retoques

--- src/Views/editAppointments.php
+++ src/Views/editAppointments.php
@@ -6,24 +6,24 @@
     <ul class="list-group lh-lg">
         <li class="list-group-item" style="border:none">
             <label>ID</label><br>
-            <input value='<?php echo $data["appointments"]->getId() ?>' class="m-0 p-0 container border border-3 border-dark" disabled="disabled">
+            <input value='<?php echo $data["appointments"]->getId() ?>' class="m-0 p-0 container border border-2 border-dark" disabled="disabled">
         </li>
         <li class="list-group-item" style="border:none">
             <label>Name</label><br>
-            <input required value='<?php echo $data["appointments"]->getName() ?>' class="m-0 p-0 container border border-3 border-dark" maxlength="20" type="text" name="name" id="name" required>
+            <input required value='<?php echo $data["appointments"]->getName() ?>' class="m-0 p-0 container border border-2 border-dark" maxlength="20" type="text" name="name" id="name" required>
         </li>
         <li class="list-group-item" style="border:none">
             <label>Topic</label><br>
-            <textarea class="m-0 p-0 form-control border border-3 border-dark" aria-label="With textarea" maxlength="50" type="text"
+            <textarea class="m-0 p-0 form-control border border-2 border-dark" aria-label="With textarea" maxlength="100" type="text"
             name="topic" id="topic" required><?php echo $data["appointments"]->getTopic() ?></textarea>
         </li>
         <li class="list-group-item" style="border:none">
             <label>Date</label><br>
-            <input value='<?php echo $data["appointments"]->getDate() ?>' class="m-0 p-0 container border border-3 border-dark" disabled="disabled">
+            <input value='<?php echo $data["appointments"]->getDate() ?>' class="m-0 p-0 container border border-2 border-dark" disabled="disabled">
         </li>
     </ul>
     
-    <div class="m-0 p-0 container btn-group-vertical position-absolute bottom-0 start-0 " role="group" aria-label="Basic mixed styles example">
+    <div class="m-0 p-0 container-fluid btn-group-vertical position-absolute bottom-0 start-0 p-2 flex-grow-1 btn bg-success" role="group" aria-label="Basic mixed styles example">
         <button  type="submit" class="p-2 flex-grow-1 btn bg-success">UPDATE APPOINTMENT</button>
     </div>
     </form>
