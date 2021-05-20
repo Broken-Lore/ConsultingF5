7e34f22b626a6ee44a77dce25279f2eddfc1a403
PPIICCAA <abdulhaidrees@gmail.com>
2021-05-20 11:44:29 +0200

    Conection Heroku

--- src/DbSession.php
+++ src/DbSession.php
@@ -19,10 +19,18 @@ class DbSession{
 
     }
     private function getConnection(){
-        $host = "localhost";
-        $user = "root";
-        $password = "";
-        $dtbase = "consulting";
+        // Local DataBase
+        //$host = "localhost";
+        //$user = "root";
+        //$password = "";
+        //$dtbase = "consulting";
+
+        // Heroku DataBase
+        $host = "eu-cdbr-west-01.cleardb.com";
+        $user = "b7e460b0197f05";
+        $password = "6b3c3055";
+        $dtbase = "heroku_e61828cc2d20c4c";
+
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
         $pdo = new PDO("mysql:host={$host}; dbname={$dtbase}", $user, $password, $options);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
