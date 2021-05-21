186e096930311311c7fa4a0dccfb9384d7253de2
anya_xo <eiekseau@gmail.com>
2021-05-21 10:46:10 +0200

    heroku conn uncommented

--- src/DbSession.php
+++ src/DbSession.php
@@ -20,16 +20,16 @@ class DbSession{
     }
     private function getConnection(){
         // Local DataBase
-        $host = "localhost";
-        $user = "root";
-        $password = "";
-        $dtbase = "consulting";
+        // $host = "localhost";
+        // $user = "root";
+        // $password = "";
+        // $dtbase = "consulting";
 
-        // // Heroku DataBase
-        // $host = "eu-cdbr-west-01.cleardb.com";
-        // $user = "b7e460b0197f05";
-        // $password = "6b3c3055";
-        // $dtbase = "heroku_e61828cc2d20c4c";
+        // Heroku DataBase
+        $host = "eu-cdbr-west-01.cleardb.com";
+        $user = "b7e460b0197f05";
+        $password = "6b3c3055";
+        $dtbase = "heroku_e61828cc2d20c4c";
 
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
         $pdo = new PDO("mysql:host={$host}; dbname={$dtbase}", $user, $password, $options);
